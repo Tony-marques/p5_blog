2b77c53b95d8a33bd9f5d6922755ef417021ceb6
tony <tony.marques@live.fr>
2023-09-18 09:42:38 +0200

    add readme file and update footer

--- templates/_partials/_footer.php
+++ templates/_partials/_footer.php
@@ -6,11 +6,16 @@ $currentDate = $date->format("Y");
 ?>
 
 <footer>
-  <div class="footer-container">
-    <?php if ($currentDate == 2023) : ?>
-      <span>© MARQUES Tony, Inc. 2023</span>
-    <?php else : ?>
-      <span>© MARQUES Tony, Inc. 2023 - <?= $currentDate ?></span>
-    <?php endif; ?>
-  </div>
+    <div class="footer-container">
+        <?php if ($currentDate == 2023) : ?>
+            <span>© MARQUES Tony, Inc. 2023</span>
+        <?php else : ?>
+            <span>© MARQUES Tony, Inc. 2023 - <?= $currentDate ?></span>
+        <?php endif; ?>
+        <div class="network">
+            <a href="https://github.com/tony-marques" target="_blank"><i class="fa-brands fa-github"></i></a>
+            <a href="www.linkedin.com/in/tony-marques1" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+        </div>
+
+    </div>
 </footer>
